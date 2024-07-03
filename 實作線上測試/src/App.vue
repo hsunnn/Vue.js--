1dc7c135fe80f4279ce3623a96317739d6bdbdf2
Hsun <pp0932451127@gmail.com>
2024-07-03 16:15:00 +0800

    first commit

---
+++ 實作線上測試/src/App.vue
@@ -0,0 +1,55 @@
+<script>
+import FormComponent from "./components/FormComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    FormComponent,
+  },
+};
+</script>
+
+<template>
+  <header>
+    <img class="logo" src="./images/前進.jpg" width="300" height="200" />
+  </header>
+
+  <main id="mainposition">
+    <TheWelcome />
+    <div id="app">
+      <FormComponent />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+  .mainposition {
+    position: center;
+  }
+}
+</style>
---
+++ 實作線上測試/src/components/FormComponent.vue
@@ -0,0 +1,92 @@
+<template>
+  <form class="form-container">
+    <div>
+      <label for="applicant">申請人：</label>
+      <input
+        type="text"
+        placeholder="李長勳"
+        id="applicant"
+        v-model="form.applicant"
+        :maxlength="10"
+        :disabled="form.locked"
+      />
+    </div>
+    <div>
+      <label for="age">年齡：</label>
+      <input
+        type="number"
+        placeholder="28"
+        id="age"
+        v-model="form.age"
+        :min="18"
+        :max="38"
+        :disabled="form.locked"
+      />
+    </div>
+    <div>
+      <label for="department">部門：</label>
+      <select id="department" v-model="form.department" :disabled="form.locked">
+        <option value="人事部">人事部</option>
+        <option value="會計部">會計部</option>
+        <option value="資訊部">資訊部</option>
+      </select>
+    </div>
+    <div>
+      <label>性別：</label>
+      <label
+        ><input
+          type="radio"
+          v-model="form.gender"
+          value="男"
+          :disabled="form.locked"
+        />
+        男</label
+      >
+      <label
+        ><input
+          type="radio"
+          v-model="form.gender"
+          value="女"
+          :disabled="form.locked"
+        />
+        女</label
+      >
+    </div>
+    <div>
+      <label><input type="checkbox" v-model="form.locked" /> 鎖定</label>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        department: "資訊部",
+        gender: "男",
+        locked: false,
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.form-container {
+  border: 1px solid darkblue;
+  border-radius: 15px;
+  padding: 50px;
+  width: 350px;
+  margin: 50px auto;
+}
+
+form div {
+  margin-bottom: 10px;
+}
+
+form label {
+  display: inline-block;
+  width: 60px;
+}
+</style>
